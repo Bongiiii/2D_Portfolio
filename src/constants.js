f888dd5deaa95d34e8d232ef215724d5e821492d
Bongiiii <136004037+Bongiiii@users.noreply.github.com>
2024-04-07 18:20:09 -0400

    project #1, 2d game based portfolio

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my workstation. I am mostly tackling some Leetcode Python interview prep questions. I am currently learning NLP from Udemy. 
+    This is my first project and it is written mostly in Javascript and incooperates Kaboom.js. Irregardless of the language, I love programming!
+    Here is my <a href="https://github.com/Bongiiii/2D_Game_based_Portfolio.git" target="_blank">Github</a>!`,
+  "Affiliations": `The following are organizations I'm part of: New Seasons Youth Program, Rewriting the Code, ColorStack, and Emzini weCode`,
+  "sofa-table": `This is my sofa, where I get to relax. I love sitting here when reading a novel, watching tv, painting or just crotcheting.
+  This is basically where all the magic happens and where I let the creative side out!`,
+  tv: `That's my TV. I love watching anime. I also have been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!
+  My all time favorite tech youtube page is freecodeCamp.org.`,
+  bed: `This is my bed. This is also where I daydream and come up with ideas.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Bongiiii/Resume.git" target="_blank">Check it out?</a>
+  Contact me at bongiwesan.mkwananzi@students.cookman.edu if you have any interesting job opportunities!`,
+  projects: `Facts about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `This is my book collection, mostly self help books. My most recent read is the Letter to my daughter by Maya Angelou. I also have a technical interview prep textbook: Cracking the Coding Interview by Gayle Laakmann McDowell, the 6th edition.`,
+  exit: `If you want to exit Bongiwe's portfolio, just close the tab.`,
+};
